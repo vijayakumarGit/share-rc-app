912ca8aa9b7ad370a3ab6db854f6115bd5e0c0bf
vijayakumarGit <vijayakumar2231@gmail.com>
2025-01-07 12:33:06 +0800

    new change yarn3

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,7 +12,9 @@ const Header = (props) => {
 
     return (
         <AppBar>
+          
             <Toolbar>
+
                 <IconButton
                     color="inherit"
                     aria-label="open drawer"
@@ -23,47 +25,52 @@ const Header = (props) => {
                 >
                     <MenuIcon />
                 </IconButton>
-                <Typography noWrap component="span" fontSize={28} fontWeight={500} >
+                <Typography noWrap component="span" fontSize={22} fontWeight={500} >
                     7
                 </Typography>
-                <StarHalfRoundedIcon sx={{ color: amber[500],fontSize: '35px', marginTop:'-24px', marginLeft:'-10px' }} />
-                <Button variant="contained" color="success" sx={{
-                        marginRight: 1,
-                    }} >
-                <Link to={'/watch-list'}> 
-                Watch List
-                </Link>
-                </Button>
+                <StarHalfRoundedIcon sx={{ color: amber[500], fontSize: '20px', marginTop: '-24px', marginLeft: '-10px' }} />
+                <Typography component="span">
+                ஸ்ரீ முருகன் துணை
+                 </Typography>
+
+                {/* <Button variant="contained" color="success" sx={{
+                    marginRight: 1,
+                    fontSize:'12px'
+                }} >
+                    <Link to={'/watch-list'}>
+                        Watch List
+                    </Link>
+                </Button> */}
                 <Hidden smDown>
-                <Button variant="contained" color="success" sx={{
+                    <Button variant="contained" color="success" sx={{
                         marginRight: 1,
                     }} >
-                
-                <Link to={'/gainers'}> 
-                Gainers
-                </Link>
-                </Button>
-                <Button variant="contained" color="error" >
-                <Link to={'/losers'}> 
-                Losers
-                </Link>
-                </Button>
-                <Button variant="contained" color="error"  sx={{
+
+                        <Link to={'/gainers'}>
+                            Gainers
+                        </Link>
+                    </Button>
+                    <Button variant="contained" color="error" >
+                        <Link to={'/losers'}>
+                            Losers
+                        </Link>
+                    </Button>
+                    <Button variant="contained" color="error" sx={{
                         marginLeft: 1,
                     }}>
-                <Link to={'/active'}> 
-                Active
-                </Link>
-                </Button>
-                <Typography noWrap component="span"  sx={{
+                        <Link to={'/active'}>
+                            Active
+                        </Link>
+                    </Button>
+                    <Typography noWrap component="span" sx={{
                         marginLeft: 1,
                     }} >
-                  Table
-                </Typography>
-                <Switch color="warning"  onChange={props.handleChange} />
-                <Typography noWrap component="span" >
-                  Chart
-                </Typography>
+                        Table
+                    </Typography>
+                    <Switch color="warning" onChange={props.handleChange} />
+                    <Typography noWrap component="span" >
+                        Chart
+                    </Typography>
                 </Hidden>
             </Toolbar>
         </AppBar>
